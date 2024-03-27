53676982d0e09626b73dc5de6e2002b47e9ab7cb
Ali ABBAS <ali.ibra.abbas@gmail.com>
2024-03-27 18:50:05 +0100

    Update slug page

--- nuxt/pages/players/[...slug].vue
+++ nuxt/pages/players/[...slug].vue
@@ -10,5 +10,24 @@
 </script>
 
 <template>
-    <pre>{{ player.data }}</pre>
+
+        <div class="">
+            <NuxtImg v-if="player?.data.image" :src="player?.data.image?.url" :alt="player?.data.slug" class="" />
+            <div class="">
+                <div class="">
+                    <h2>{{player?.data.first_name}} {{player?.data.last_name}}</h2>
+                    <div>Ranking: {{player?.data.ranking}}</div>
+                </div>
+                <div class="">
+                    <span v-if="player?.data.competitions && player.data.competitions.length >= 1">Compétition:</span>
+                    <span v-else>Aucune compétition:</span>
+                    <div v-for="(competition, index) in player?.data.competitions" :key="index">
+                        <div v-if="competition">
+                            {{ competition.name }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 </template>
